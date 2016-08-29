00c4fdc310d3f53c7d4fe3ecdcbef9e49c2cc049
duzeyu <duzeyu@baidu.com>
2016-08-29 13:38:33 +0800

    1.0.3

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ we need a anjularjs-directive to download file from client.
 This little project is to create a easy way to export data to ngExcel and download;
 
 ## CHANGES
-##V1.0
+##V1.01
 ### ~ support muti-sheet
 ### ~ support utf-8 [chinese^-^]
-##V1.1
+##V1.02
 ### ~ support big file by blob.
+##V1.03
+### ~ add test.html[to see how to ues]
 
 #how to use .
 
@@ -20,27 +22,52 @@ change
 'XXX' to your module name.
 		
 ## Second: 
-方法1：支持图标，点击下载。
-```js
-	$scope.data = 
-	{
-        	name: 'no data',
-        	data: [['name','sex'],['Tom','Harry']
-        }
+Usage
 ```
-```html
-	<div ng-excel="" type="'excel'" data="data" xlsfilename="'media_wave_data.xls'" >
-```
-方法2: 支持函数，可以想怎么做就怎么做。
-```
- $scope.toolsbox.click = function (v) {
-    var table = [....];
-    $scope.excleTools.data = data;
-    $scope.excleTools.down(data);
-};             
-<div class="download" ng-show="false">
-    <div ng-excel="" type="'excel'" data="excleTools.data" xlsfilename="'dashboard.xls'" down="excleTools.down">
+<!doctype html>
+<html ng-app="myapp">
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>   
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-sanitize.min.js"></script>   
+    <script src="./ngExcel.js"></script>
+</head>
+<body>
+
+<div >
+    <div class="container" ng-controller="myctrl">
+      <div class="download">
+            <div ng-excel="" type="'excel'" data="excel.data" xlsfilename="'data.xls'" down="excel.down">
+            </div>
+       <span ng-click="excel.down()">download</span>
     </div>
-</div>               
+</div>
+
+<script>
+    (function () {
+        var myapp = angular.module('myapp', ['ngExcel']);
+
+        myapp.controller('myctrl', function ($scope) {
+           $scope.excel = {
+                down: function () {
+                },
+                data: [{
+                    name: 'sheet1',
+                    data: 
+                        [[1,2,3],['hello','ng-','excel']]
+                }, {
+                    name: 'sheet2',
+                    data: [
+                        ['data = [{name:\'sheetName\',data:[[]]}]'],
+                        ['xlsfilename=fileName']
+                    ]
+                }]
+           };
+        });
+    })();
+</script>
+
+</body>
+</html>
+            
 ```               
 ## Dep: anjularjs 1.4.2
--- ngExcel.js
+++ ngExcel.js
@@ -4,9 +4,10 @@
  * @email http://dozy.me
  */
 
-(function () {
+(function(window, document) {
     'use strict';
-    angular.module('angularGulp').directive('ngExcel', [
+    var ngExcelModule = angular.module('ngExcel', []);
+    ngExcelModule.directive('ngExcel', [
         '$q',
         ngExcel
     ]);
@@ -16,27 +17,26 @@
             restrict: 'AE',
             require: '',
             scope: {
-                // 类型
+                // fileTyle excle
                 type: '=type',
-                // 数据
+                // data
                 data: '=data',
-                // 下载文件名
+                // fileName
                 xlsfilename: '=xlsfilename',
-                // 下载
+                // down
                 down: '=?down'
             },
-            template: '<a class="ng-excel" download="test.xls" ng-click="exportTo(type, data)">'
-            + '<span class="glyphicon glyphicon-download-alt" ></span></a>',
-            link: function (scope, elm, attrs) {
+            template: '<a class="ng-excel" download="test.xls" ng-click="exportTo(type, data)">' + '<span></span></a>',
+            link: function(scope, elm, attrs) {
                 var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
                 var fromCharCode = String.fromCharCode;
                 var self = window;
-                var toString = function (a) {
+                var toString = function(a) {
                         return String(a);
-                }
-                // 兼容不容浏览器
+                    }
+                    // different brower
                 var myBlob = (self.Blob || self.MozBlob || self.WebKitBlob || toString);
-                var ExcelExport = function () {
+                var ExcelExport = function() {
                     var version = '1.3';
                     var csvSeparator = ',';
                     var uri = {
@@ -44,35 +44,37 @@
                         csv: 'data:application/csv;base64,'
                     };
                     var template = {
-                        excel: '<?xml version="1.0"?><?mso-application progid="Excel.Sheet"?>'
-                        + '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" '
-                        + 'xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">'
-                        + '{worksheets}</Workbook>',
+                        excel: '<?xml version="1.0"?><?mso-application progid="Excel.Sheet"?>' + '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" ' + 'xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">' + '{worksheets}</Workbook>',
                         worksheet: '<Worksheet ss:Name="{name}"><Table>{rows}</Table></Worksheet>',
                         cell: '<Cell><Data ss:Type="{type}">{data}</Data></Cell>'
                     };
                     var csvDelimiter = ',';
                     var csvNewLine = '\r\n';
-                    var base64 = function (s) {
+                    var base64 = function(s) {
                         return window.btoa(unescape(encodeURIComponent(s)));
                     };
-                    var format = function (s, c) {
-                        return s.replace(new RegExp('{(\\w+)}', 'g'), function (m, p) {
+                    var format = function(s, c) {
+                        return s.replace(new RegExp('{(\\w+)}', 'g'), function(m, p) {
                             return c[p];
                         });
                     };
-                    
-                    var excel = function (data) {
+
+                    var excel = function(data) {
                         var worksheets = '';
                         var ctx = {};
-                        // 容错
+                        // fault-tolerant
                         if ((typeof(data) === 'undefined') || (data.length === 0)) {
                             data = [{
-                                name: '无数据',
-                                data: [['无数据，请查询后再下载~']]
+                                name: 'no data',
+                                data: [
+                                    ['no data']
+                                ]
                             }, {
-                                name: '使用说明',
-                                data: [['data = [{name:\'表名\',data:[[]]}]'], ['xlsfilename=输出文文件名']]
+                                name: 'readMe',
+                                data: [
+                                    ['data = [{name:\'sheetName\',data:[[]]}]'],
+                                    ['xlsfilename=fileName']
+                                ]
                             }];
                         }
                         for (var x in data) {
@@ -80,12 +82,14 @@
                             var sheetContent = '';
                             var sheetName = sheet.name;
                             var sheetData = sheet.data;
-                            // 容错
+                            // fault-tolerant
                             if (typeof(sheetName) === 'undefined') {
                                 sheetName = 'data';
                             }
                             if ((typeof(data) === 'undefined') || (data.length === 0)) {
-                                sheetData = [['no data']];
+                                sheetData = [
+                                    ['no data']
+                                ];
                             }
                             var tableContent = '';
                             for (var y in sheetData) {
@@ -94,7 +98,10 @@
                                 for (var z in table) {
                                     var row = table[z];
                                     var type = (!isNaN(Number(row))) ? 'Number' : 'String';
-                                    ctx = {data: row, type: type};
+                                    ctx = {
+                                        data: row,
+                                        type: type
+                                    };
                                     rowContent += format(template.cell, ctx);
                                 }
                                 rowContent += '</Row>';
@@ -117,44 +124,44 @@
                 };
                 var exportTools = new ExcelExport();
 
-                var doClick = function (url) {
+                var doClick = function(url) {
                     // data to blob
-                    var dataUrlToBlob = function (strUrl) {
-                        var parts= strUrl.split(/[:;,]/),
-                        type = parts[1],
-                        decoder = (parts[2] === "base64") ? atob : decodeURIComponent,
-                        binData = decoder(parts.pop()),
-                        mx = binData.length,
-                        i = 0,
-                        uiArr = new Uint8Array(mx);
-                        for(i;i < mx; ++i)
-                            {
-                                uiArr[i]= binData.charCodeAt(i);
-                            }
-                        return new myBlob([uiArr], {type: 'application/vnd.ms-excel'});
+                    var dataUrlToBlob = function(strUrl) {
+                        var parts = strUrl.split(/[:;,]/),
+                            type = parts[1],
+                            decoder = (parts[2] === "base64") ? atob : decodeURIComponent,
+                            binData = decoder(parts.pop()),
+                            mx = binData.length,
+                            i = 0,
+                            uiArr = new Uint8Array(mx);
+                        for (i; i < mx; ++i) {
+                            uiArr[i] = binData.charCodeAt(i);
+                        }
+                        return new myBlob([uiArr], {
+                            type: 'application/vnd.ms-excel'
+                        });
                     }
                     var anchor = document.createElement('a');
-                    if (typeof (scope.xlsfilename) === 'undefined') {
+                    if (typeof(scope.xlsfilename) === 'undefined') {
                         scope.xlsfilename = 'res.xls';
                     }
-                    if (url.length < 1024*1024*1.999) {
+                    if (url.length < 1024 * 1024 * 1.999) {
                         if ('download' in anchor) { //html5 A[download]
-                                anchor.href = url;
-                                anchor.setAttribute("download", scope.xlsfilename);
-                                anchor.className = "download-js-link";
-                                anchor.innerHTML = "downloading...";
-                                anchor.style.display = "none";
-                                document.body.appendChild(anchor);
-                                setTimeout(function() {
-                                    anchor.click();
-                                    document.body.removeChild(anchor);
-                                }, 1000);
-                                return true;
-                            }
+                            anchor.href = url;
+                            anchor.setAttribute("download", scope.xlsfilename);
+                            anchor.className = "download-js-link";
+                            anchor.innerHTML = "downloading...";
+                            anchor.style.display = "none";
+                            document.body.appendChild(anchor);
+                            setTimeout(function() {
+                                anchor.click();
+                                document.body.removeChild(anchor);
+                            }, 1000);
+                            return true;
+                        }
                     } else {
-                        // excel 文件比较大的时候使用blob进行二进制编解码.
-                        // support big file ^-^
-                        if(myBlob !== toString ){
+                        // support big file by blob
+                        if (myBlob !== toString) {
                             var blob = dataUrlToBlob(url);
                             url = URL.createObjectURL(blob);
                             if ('download' in anchor) { //html5 A[download]
@@ -173,7 +180,7 @@
                         }
                     }
                 };
-                scope.exportTo = function (type, data) {
+                scope.exportTo = function(type, data) {
                     scope.url = '';
                     if (type === 'excel') {
                         var e = exportTools(data);
@@ -182,7 +189,7 @@
                     }
 
                 };
-                scope.down = function (data) {
+                scope.down = function(data) {
                     var data = data || scope.data;
                     var type = scope.type;
                     scope.exportTo(type, data);
@@ -190,4 +197,4 @@
             }
         };
     }
-})();
+})(window, document);
